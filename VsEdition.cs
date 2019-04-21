81cf15d9bc1b1b4807ae94c321196b22d60faeac
Vitalij Shitts <vs@vitevic.com>
2019-04-21 14:33:57 +0300

    initial commit to create a repo

---
+++ VsEdition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitevic.Installer
+{
+    enum VsEdition
+    {
+        None = 0, // for build tools
+        Community,
+        Professional,
+        Enterprise,
+    }
+}
